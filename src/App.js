3ceb98de34bf5c8d7ca811907e3db015f1cc0ab7
Jrodr4544 <maskdbandit@gmail.com>
2018-12-08 00:26:32 +0000

    removed default contents and added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+
+import NavBar from './components/NavBar';
+
+//  Default React stuff
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,19 +10,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="navbar">
+          <p>NavBar</p>
+          <NavBar />
+        </div>
+
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
