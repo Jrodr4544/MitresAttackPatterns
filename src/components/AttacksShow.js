651fc586a155ac201a9a94ff9d8c811052eabe00
Jrodr4544 <maskdbandit@gmail.com>
2018-12-09 21:00:33 +0000

    initial commit for attack show page

---
+++ src/components/AttacksShow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AttacksShow = () => {
+    return (
+        <div>
+            <h1>Attacks Show Page</h1>
+        </div>
+    )
+}
+
+export default AttacksShow;
