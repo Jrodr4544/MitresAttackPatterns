c3fea1a443a4efb13f7b408b1980d60d30ee828a
Jrodr4544 <maskdbandit@gmail.com>
2018-12-09 20:53:57 +0000

    renamed from AttackList

---
+++ src/components/AttacksList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const AttacksList = ({ attacks }) => {
+    {/* AttackPatterns will be rendered here */}
+    debugger
+  //const renderAttacks;
+  // = attacks.map(attack => 
+  //  <Link style={{ marginRight: '12px' }} key={attack.id} to={`/attacks/${attack.id}`}>{attack.name}</Link>
+  //);
+  
+  return (
+    <div>
+        <h1> Attacks List </h1>
+      {/*renderAttacks*/}
+    </div>
+  );
+};
+
+export default AttacksList;
