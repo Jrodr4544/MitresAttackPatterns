bb168678561a6ab5da8c223ef616980a67f18839
Jrodr4544 <maskdbandit@gmail.com>
2018-12-09 17:46:53 +0000

    added router functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import { 
+  BrowserRouter as Router, 
+  Route 
+} from 'react-router-dom';
 
 import NavBar from './components/NavBar';
+import About from './components/About';
 
 //  Default React stuff
 import logo from './logo.svg';
@@ -10,7 +15,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
+        <Router>
+          <div>
+            <NavBar />
+              <Route exact path="/" render={() => <div>Home</div>} />
+              {/* <Route path='/attacks' component={AttacksPage}/> */}
+              <Route path='/about' component={About}/>
+          </div>
+        </Router>
 
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
