f419f1ccd03e6491dfd0fa80c93c36564fa2f82e
Jrodr4544 <maskdbandit@gmail.com>
2018-12-11 20:05:41 +0000

    grabbing unique attack based on id from state and rendering it along with it's details

--- src/components/AttacksShow.js
+++ src/components/AttacksShow.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Panel } from 'react-bootstrap';
 
-const AttacksShow = () => {
+const AttacksShow = props => {
+debugger
+
     return (
-        <div>
-            <h1>Attacks Show Page</h1>
-        </div>
-    )
-}
+      <div>
+        <Panel>
+          <Panel.Heading>{ props.attackPattern.name }</Panel.Heading>
+          <Panel.Body className="panelText">{ props.attackPattern.description }</Panel.Body>
+          <h4>References:</h4>
+          <ul className="panelText">
+              { props.references }
+          </ul>
+        </Panel>
+      </div>
+    );
+};
 
-export default AttacksShow;
+const mapStateToProps = (state, ownProps) => {
+    const attackPattern = state.attackPatterns.find(attack => attack.id == ownProps.match.params.attackId)
+ debugger 
+
+    if (attackPattern) {
+      let references = attackPattern.external_references.map( reference => <li>Source: { reference.source_name } | <a href={ reference.url }>{ reference.url }</a></li>);
+
+      return { references, attackPattern }
+    } else {
+      return { attackPattern: {} }
+    }
+  }
+  
+  export default connect(mapStateToProps)(AttacksShow);
