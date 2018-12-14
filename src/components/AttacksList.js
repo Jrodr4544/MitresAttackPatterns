3ed5d9e8bc69635b57d4e36215e01d9dc65cd599
Jrodr4544 <maskdbandit@gmail.com>
2018-12-14 20:00:42 +0000

    added attack name to panel title and moved attackPattern link from title

--- src/components/AttacksList.js
+++ src/components/AttacksList.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { PanelGroup, Panel } from 'react-bootstrap';
 
+
 const AttacksList = ({ attacks }) => {
     {/* AttackPatterns will be rendered here */}
     debugger
   const renderAttacks = attacks.map( attack => 
-    <PanelGroup accordion id="accordion-example">
+    <PanelGroup accordion id="accordion-example" key={attack.id}>
       <Panel eventKey={attack.id}>
         <Panel.Heading>
           <Panel.Title toggle>
-            <Link style={{ marginRight: '12px' }} key={attack.id} to={`/attacks/${attack.id}`}>{attack.name}</Link>
+            { attack.name }
           </Panel.Title>
+            <br></br>
+            <Link style={{ marginRight: '12px' }} key={attack.id} to={`/attacks/${attack.id}`}>View Pattern Details</Link>
         </Panel.Heading>
-        <Panel.Body className='panel-text' collapsible>
+        <Panel.Body className="panelText" collapsible>
           {attack.description}
         </Panel.Body>
       </Panel>
