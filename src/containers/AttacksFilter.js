b9d78eda3771be04c1601173929e7bab0568ce12
Jrodr4544 <maskdbandit@gmail.com>
2018-12-14 20:17:09 +0000

    added actions, defined form and select options for filtering, added filter options to state; option filtering functioning properly

--- src/containers/AttacksFilter.js
+++ src/containers/AttacksFilter.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { fetchFilters, fetchAttackPatterns, filterAttackPatterns } from '../actions';
 import { connect } from 'react-redux';
-import { filterAttackPatterns } from '../actions';
 import { FormGroup, InputGroup, FormControl, Button } from 'react-bootstrap';
+import AttacksList from '../components/AttacksList';
+
 
 class AttacksFilter extends Component {
   constructor(props) {
@@ -9,13 +11,22 @@ class AttacksFilter extends Component {
     this.state = {
       name: '',
       description: '',
+      datasourceFilter: null,
+      platformFilter: null,
+      filteredAttacks: props.attackPatterns
     };
+  }
+
+  componentDidMount() {
+      debugger
+    this.props.fetchAttackPatterns()
+    this.props.fetchFilters()
   }
 
   handleOnSubmit = event => {
     event.preventDefault();
-//    const { addPet, history } = this.props
-//    filterAttackPatterns(this.state); // this needs to be a defined function that dispatches to reducer from the actions file
+    debugger
+    filterAttackPatterns(this.state); // this needs to be a defined function that dispatches to reducer from the actions file
 //    history.push('/pets'); // might need to use this
   }
 
@@ -23,30 +34,72 @@ class AttacksFilter extends Component {
 
   }
 
-  handleOnChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
+// gotta work on the filter functionality
+  handleOnFilterChange = event => {
+    var filter = {[event.target.name]: event.target.value}
+
+    this.setState(
+      filter
+    );
+debugger
+
+    this.props.filterAttackPatterns(filter)
   }
 
   render() {
+    debugger
+
     return (
       <div>
-        <h2>Filter Attack Patterns</h2>
-
         <form onSubmit={this.handleOnSubmit} >
-            <FormGroup>
-                <InputGroup>
-                    <InputGroup.Button>
-                        <Button type='submit' value="Filter Attack Patterns">Filter Attack Patterns</Button>
-                    </InputGroup.Button>
-                    <FormControl type="text" onChange={this.handleOnChange}/>
-                </InputGroup>
-            </FormGroup>
+          <FormGroup>
+              <InputGroup>
+                  <InputGroup.Button>
+                      <Button type='submit' value="Filter Attack Patterns">Filter Attack Patterns</Button>
+                  </InputGroup.Button>
+                  <FormControl name="name" type="text" onChange={this.handleOnChange}/>
+              </InputGroup>
+          </FormGroup>
         </form>
+
+          { this.props.filters.data_sources !== undefined ? (
+
+            <div name="filters"> 
+              <select name="datasourceFilter" onChange={this.handleOnFilterChange} defaultValue='all'>
+                <option value='all'>All</option>
+                  {this.props.filters.data_sources.map(filter =>
+                    <option key={filter} value={filter}>{filter}</option>
+                  )}
+              </select>
+
+              <select name="platformFilter" onChange={this.handleOnFilterChange} defaultValue='all'>
+                <option value='all'>All</option>
+                  {this.props.filters.platforms.map(filter =>
+                    <option key={filter} value={filter}>{filter}</option>
+                  )}
+              </select>
+            </div>
+
+          ) : (
+
+            <div>
+              Filters
+            </div>
+
+          )}
+            <br></br>
+        <AttacksList attacks={ this.props.attackPatterns } />
       </div>
     );
   }
 };
 
-export default connect(null, { filterAttackPatterns })(AttacksFilter);
+const mapStateToProps = state => {
+ debugger
+ return {
+  attackPatterns: state.attackPatterns,
+  filters: state.filters
+ };
+}
+
+export default connect(mapStateToProps, { fetchFilters, fetchAttackPatterns, filterAttackPatterns })(AttacksFilter);
