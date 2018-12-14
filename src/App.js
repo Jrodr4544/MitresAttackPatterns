778d6e16455640d9d8b3308d79068eaba22d9661
Jrodr4544 <maskdbandit@gmail.com>
2018-12-14 19:53:40 +0000

    added AttacksPage container to App in order to render on specified route

--- src/App.js
+++ src/App.js
@@ -7,20 +7,26 @@ import {
 import NavBar from './components/NavBar';
 import About from './components/About';
 
+import AttacksPage from './containers/AttacksPage';
+
 //  Default React stuff
-import logo from './logo.svg';
+import logo from './mitre_attack.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      
         <Router>
           <div>
             <NavBar />
-              <Route exact path="/" render={() => <div>Home</div>} />
-              {/* <Route path='/attacks' component={AttacksPage}/> */}
+              <Route exact path="/" render={() => (
+                <div className="App-welcome"><h1>Welcome</h1></div>
+              )}/>
+              <Route path='/attacks' component={AttacksPage}/>
               <Route path='/about' component={About}/>
+
           </div>
         </Router>
       </div>
