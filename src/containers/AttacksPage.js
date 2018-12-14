40e347b9d9c50f2b61c33d76a241c060f7e0cf9a
Jrodr4544 <maskdbandit@gmail.com>
2018-12-14 20:28:25 +0000

    moved fetchAttackPatterns function over to AttacksFilter container

--- src/containers/AttacksPage.js
+++ src/containers/AttacksPage.js
@@ -1,38 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
+
 import { fetchAttackPatterns } from '../actions/index';
-//import AttacksNew from './PetsNew';
+
 import AttacksShow from '../components/AttacksShow';
 import AttacksList from '../components/AttacksList';
-import AttacksFilter from './AttacksFilter'
 
-import logo from '../mitre_attack.png'
+import AttacksFilter from './AttacksFilter';
+
+//import logo from '../mitre_attack.png'
 
 class AttacksPage extends Component {
 
-  componentDidMount() {
-      debugger
-    this.props.fetchAttackPatterns()
-  }
+  //componentDidMount() {
+  //    debugger
+  //  this.props.fetchAttackPatterns()
+  //}
 
   render() {
       debugger
-    const { match, attackPatterns } = this.props;
+    const { match } = this.props;//, attackPatterns } = this.props;
 
     return (
      <div>
       {/* Attacks Page */}
-      <AttacksFilter />
-       <Switch>  {/* Make sure to wrap all of your Routes as children of the Switch component*/ }
+      <AttacksFilter/>{/* attackPatterns={attackPatterns}/> */}
+
+        <Switch>  {/* Make sure to wrap all of your Routes as children of the Switch component*/ }
          <Route exact path={match.url} render={() => ( 
            <h3>Select an Attack from the list.</h3> 
          )}/> 
          <Route path={`${match.url}/:attackId`} component={AttacksShow}/> 
 
-       </Switch> 
-
-      <AttacksList attacks={ attackPatterns } />
+        </Switch> 
      </div>
     )
   }
@@ -45,4 +46,5 @@ const mapStateToProps = state => {
   };
 }
 
-export default connect(mapStateToProps, { fetchAttackPatterns })(AttacksPage);
+export default connect(null)(AttacksPage);
+//export default connect(mapStateToProps)(AttacksPage);//, { fetchAttackPatterns })(AttacksPage);
