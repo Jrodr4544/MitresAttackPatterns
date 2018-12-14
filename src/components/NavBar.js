ef5059ffd94d5ae180a5e39879c501c01f1dc576
Jrodr4544 <maskdbandit@gmail.com>
2018-12-14 20:13:52 +0000

    removed eventKey attribute, href, and styling for NavLinks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,45 +2,40 @@ import React from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap'; 
 import { NavLink } from 'react-router-dom';
 
-
 const NavBar = () => {
-
     return (
-
-<Navbar inverse collapseOnSelect>
-  <Navbar.Header>
-    <Navbar.Brand>
-      Mitre's Att&ck Patterns
-    </Navbar.Brand>
-    <Navbar.Toggle />
-  </Navbar.Header>
-  <Navbar.Collapse>
-    <Nav>
-      <NavItem eventKey={1} >
-        <NavLink
-           style={{ marginRight: '10px' }} 
-           to="/attacks"
-        > 
-          Attacks 
-        </NavLink>
-      </NavItem>
-    </Nav>
-    <Nav pullRight>
-      <NavItem eventKey={1} >
-        <NavLink
-          style={{ marginRight: '10px' }} 
-          to="/about"
-        > 
-          About 
-        </NavLink>
-      </NavItem>
-      {/*<NavItem eventKey={2} href="#">
-        Link Right
-    </NavItem>*/}
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <NavLink
+              to='/'
+            >
+              Mitre's Att&ck Patterns
+            </NavLink>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem>
+              <NavLink
+                to="/attacks"
+              > 
+                Attacks 
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavItem>
+              <NavLink
+                to="/about"
+              > 
+                About 
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     )
 }
 
