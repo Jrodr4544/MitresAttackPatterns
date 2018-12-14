53ab6e6117bb682a0ecd17104502546cccbdd7ad
Jrodr4544 <maskdbandit@gmail.com>
2018-12-14 19:58:42 +0000

    removed return and unnecessary div

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,8 @@
 import React from 'react'; 
 
-const About = () => {
-    return (
-        <div>
-            <h1>About page</h1>
-        </div>
-    );
-}
+const About = () => 
+        <h1>
+            About Page
+        </h1>
 
 export default About;
