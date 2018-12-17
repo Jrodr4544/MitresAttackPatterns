df1186a35fb073fbbcf0db2e2813ae9ac1762329
Jrodr4544 <maskdbandit@gmail.com>
2018-12-17 04:09:15 +0000

    modified attacksPage as a stateless component

--- src/containers/AttacksPage.js
+++ src/containers/AttacksPage.js
@@ -2,47 +2,33 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 
-import { fetchAttackPatterns } from '../actions/index';
+import FilterContainer from './FilterContainer';
 
 import AttacksShow from '../components/AttacksShow';
-import AttacksList from '../components/AttacksList';
-
-import AttacksFilter from './AttacksFilter';
 
 //import logo from '../mitre_attack.png'
 
+
 class AttacksPage extends Component {
-
-  //componentDidMount() {
-  //    debugger
-  //  this.props.fetchAttackPatterns()
-  //}
-
+//const AttacksPage = ({match}) => {
   render() {
       debugger
-    const { match } = this.props;//, attackPatterns } = this.props;
+    const { match } = this.props;
 
     return (
-     <div>
-      {/* Attacks Page */}
-      <AttacksFilter/>{/* attackPatterns={attackPatterns}/> */}
+      <div>
+{/* Attacks Page */}
+      <FilterContainer />
 
-        <Switch>  {/* Make sure to wrap all of your Routes as children of the Switch component*/ }
-         <Route exact path={match.url} render={() => ( 
-           <h3>Select an Attack from the list.</h3> 
+        <Switch>  
+         <Route path={`${match.url}/:attackId`} component={AttacksShow}/> 
+         <Route path={match.url} render={() => ( 
+          <h3>Select an Attack from the list.</h3>
          )}/> 
         </Switch> 
-     </div>
+      </div>
     )
   }
 };
 
-const mapStateToProps = state => {
-    debugger
-  return {
-    attackPatterns: state.attackPatterns
-  };
-}
-
 export default connect(null)(AttacksPage);
-//export default connect(mapStateToProps)(AttacksPage);//, { fetchAttackPatterns })(AttacksPage);
