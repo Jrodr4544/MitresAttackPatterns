7ffe862e09531a5b9d1af537eb91a6565af26481
Jrodr4544 <maskdbandit@gmail.com>
2018-12-17 22:00:12 +0000

    added an img tag with the app's logo to the welcome page

--- src/App.js
+++ src/App.js
@@ -6,12 +6,10 @@ import {
 
 import NavBar from './components/NavBar';
 import About from './components/About';
-
-import AttacksPage from './containers/AttacksPage';
+import AttacksPage from './components/AttacksPage';
 
 //  Default React stuff
 import logo from './mitre_attack.png';
-import './App.css';
 
 class App extends Component {
   render() {
@@ -23,7 +21,8 @@ class App extends Component {
               <Route path='/attacks' component={AttacksPage}/>
               <Route path='/about' component={About}/>
               <Route exact path="/" render={() => (
-                <div className="App-welcome"><h1>Welcome</h1></div>
+                <div className="App"><h1>Welcome</h1><img className="App-logo" src={logo} alt='171x180' /></div>
+
               )}/>
           </div>
         </Router>
