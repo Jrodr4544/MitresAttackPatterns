1a52505fcae299505d14b04fa774e76da6545ce2
Jrodr4544 <maskdbandit@gmail.com>
2019-01-06 20:06:13 +0000

    initialized attack_patterns_controller with an index and create action

---
+++ app/controllers/api/attack_patterns_controller.rb
@@ -0,0 +1,24 @@
+class Api::AtttackPatternsController < ApplicationController
+
+    def index
+        #binding.pry
+        @attackPatterns = AttackPattern.all
+        render json: @attackPatterns
+    end
+
+    def create
+        @attack_pattern = AttackPattern.new(attack_pattern_params)
+        
+    end
+
+    private
+  
+        def set_attack_pattern
+         @attackPattern = AttackPattern.find(params[:id])
+        end
+
+        def attack_pattern_params 
+            params.require(:attack_pattern).permit!
+        end
+
+end
