a05d98d8adb0bfb7df0b58ad114315b8f4246f90
Jrodr4544 <maskdbandit@gmail.com>
2019-01-08 23:35:17 +0000

    modified store to use compose from redux in order to use multiple enhancers such as, ReduxDevTools and thunk as middleware

--- src/store.js
+++ src/store.js
@@ -1,8 +1,15 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers/index'
+import thunk from 'redux-thunk'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // add other store enhancers if any
+);
 
 export function configureStore(){
-  return createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  return createStore(rootReducer, enhancer)
 }
 
 const store = configureStore()
