a72fc7c065dacfcea01393987332b949570511bd
Jrodr4544 <maskdbandit@gmail.com>
2019-01-08 23:28:31 +0000

    added loading action to update state property, finalized Fetch action, and added an action that cats called on error

--- src/reducers/attacksReducer.js
+++ src/reducers/attacksReducer.js
@@ -1,11 +1,24 @@
-export default (state = [], action) => {
-    let idx;
-    switch (action.type) {
-        case "FETCH_ATTACK_PATTERNS":
+export default (state = {loading: false, patterns: []}, action) => {
 
-            return action.attackPatterns.objects
+    switch (action.type) {
+        case "LOADING_ATTACK_PATTERNS":
+        debugger
+		console.log('LOADING_ATTACK_PATTERNS')
+            return { ...state, loading: true }
+
+        case "FETCH_ATTACK_PATTERNS":
+        debugger
+        console.log('FETCHING_ATTACK_PATTERNS')
+        console.log(action)
+            return { ...state, patterns: action.attackPatterns }
+
+        case "FETCH_ERROR_GENERATED":
+        console.log('FETCH_ERROR_GENERATED')
+        console.log(action)
+            return { ...state, loading: action } 
 
         default:
             return state
     }
+
 }
