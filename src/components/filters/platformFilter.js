5676187f15feeeef26a51729d02284420a89faee
Jrodr4544 <maskdbandit@gmail.com>
2019-01-12 02:12:34 +0000

    removed defaultValue and changed the All option value from All to blank

--- src/components/filters/platformFilter.js
+++ src/components/filters/platformFilter.js
@@ -5,8 +5,8 @@ const PlatformFilter = ({platforms, changeFilter}) => {
     const filterOptions = platforms.map((filter, index) => <option key={index} value={filter}>{filter}</option>)
 
     return (
-        <select name="platformFilter" onChange={changeFilter} defaultValue='all'>
-            <option value='all'>All</option>
+        <select name="platformFilter" onChange={changeFilter}>
+            <option value=''>All</option>
             {filterOptions}
         </select>
     );
