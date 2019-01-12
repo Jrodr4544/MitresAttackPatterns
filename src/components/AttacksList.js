b6b520e218f1fee4e69baaf88c18f8c99ba98456
Jrodr4544 <maskdbandit@gmail.com>
2019-01-12 01:11:47 +0000

    removed filtering functionality from AttacksList component

--- src/components/AttacksList.js
+++ src/components/AttacksList.js
@@ -3,26 +3,9 @@ import { Link } from 'react-router-dom';
 import { PanelGroup, Panel } from 'react-bootstrap';
 
 
-const AttacksList = ({ attacks, datasourceFilter, platformFilter, textFilter }) => {
+const AttacksList = ({ attackPatterns }) => {
     {/* AttackPatterns will be rendered here */}
-    debugger
 
-  let attackPatterns = attacks;
-  
-  if ( textFilter ) {
-    attackPatterns = attackPatterns.filter (
-      attack => Object.values(attack).flat().join('').includes(textFilter)
-    )
-  }
-
-  if ( datasourceFilter !== 'all') {
-    attackPatterns = attackPatterns.filter( attack => attack.x_mitre_data_sources.includes(datasourceFilter));
-  } 
-
-  if (platformFilter !== 'all') {
-    attackPatterns = attackPatterns.filter( attack => attack.x_mitre_platforms.includes(platformFilter));
-  } 
-  
   const renderAttacks = attackPatterns.map( attack => 
     <PanelGroup accordion id="accordion-example" key={attack.id}>
       <Panel eventKey={attack.id}>
