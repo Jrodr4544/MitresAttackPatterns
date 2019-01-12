ff054c4970478e4bef48d85535599effd6dc6a19
Jrodr4544 <maskdbandit@gmail.com>
2019-01-12 02:06:42 +0000

    removed defaultValue and changed the All option value from All to blank

--- src/components/filters/datasourceFilter.js
+++ src/components/filters/datasourceFilter.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const DatasourceFilter = ({datasources, changeFilter}) => {
-    debugger
+    // debugger
     const filterOptions = datasources.map((filter, index) => <option key={index} value={filter}>{filter}</option>)
 
     return (
-            <select name="datasourceFilter" onChange={changeFilter} defaultValue='all'>
-                <option value='all'>All</option>
+            <select name="datasourceFilter" onChange={changeFilter}> {/* defaultValue='all'> */}
+                <option value=''>All</option>
                 {filterOptions}
             </select>
     );
