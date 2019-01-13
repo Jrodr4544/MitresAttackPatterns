6065d02dedecd89f8e1d1f67e75f8b0e81464f20
Jrodr4544 <maskdbandit@gmail.com>
2019-01-13 21:47:56 +0000

    added a class to select element

--- src/components/filters/datasourceFilter.js
+++ src/components/filters/datasourceFilter.js
@@ -5,10 +5,10 @@ const DatasourceFilter = ({datasources, changeFilter}) => {
     const filterOptions = datasources.map((filter, index) => <option key={index} value={filter}>{filter}</option>)
 
     return (
-            <select name="datasourceFilter" onChange={changeFilter}> {/* defaultValue='all'> */}
-                <option value=''>All</option>
-                {filterOptions}
-            </select>
+          <select componentClass="select" name="datasourceFilter" onChange={changeFilter}>
+            <option value="">All</option>
+            { filterOptions }
+          </select>
     );
 }
 
