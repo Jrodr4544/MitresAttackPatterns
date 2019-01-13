f06d1ae581e446388da8d4fc00b408dea43678b2
Jrodr4544 <maskdbandit@gmail.com>
2019-01-13 21:48:15 +0000

    added a class to select element

--- src/components/filters/platformFilter.js
+++ src/components/filters/platformFilter.js
@@ -5,10 +5,10 @@ const PlatformFilter = ({platforms, changeFilter}) => {
     const filterOptions = platforms.map((filter, index) => <option key={index} value={filter}>{filter}</option>)
 
     return (
-        <select name="platformFilter" onChange={changeFilter}>
-            <option value=''>All</option>
-            {filterOptions}
-        </select>
+          <select componentClass="select" name="platformFilter" onChange={changeFilter}>
+            <option value="">All</option>
+            { filterOptions }
+          </select>
     );
 }
 
