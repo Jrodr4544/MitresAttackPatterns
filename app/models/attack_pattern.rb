5c7f0ec5585b754f49d99134afe9043877cf17f3
Jrodr4544 <maskdbandit@gmail.com>
2019-02-26 20:21:53 +0000

    added a has_many comments relationship and removed the unused definition serialization

--- app/models/attack_pattern.rb
+++ app/models/attack_pattern.rb
@@ -2,8 +2,7 @@ class AttackPattern < ApplicationRecord
     self.primary_key = 'id'
     self.inheritance_column = :_type_disabled
 
-    serialize :definition
-
     has_many :external_references
     has_many :kill_chain_phases
+    has_many :comments
 end
