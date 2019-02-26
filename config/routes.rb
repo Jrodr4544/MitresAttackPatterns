79dad4a9b17e521deece232c17b10504989ebda7
Jrodr4544 <maskdbandit@gmail.com>
2019-02-26 20:20:57 +0000

    added the specific post route for adding new comments to the attack_patterns resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    resources :attack_patterns
+    resources :attack_patterns do
+      member do
+        post 'add_comment'
+      end
+    end
+    # resources :comments, only: %i[create]
   end
 end
