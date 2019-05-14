e6c967f8404e064d4a59db9bcc054085524f47b1
jrodr4544 <maskdbandit@gmail.com>
2019-05-14 09:52:20 -0400

    removed unused action and commented code

--- app/controllers/api/attack_patterns_controller.rb
+++ app/controllers/api/attack_patterns_controller.rb
@@ -1,36 +1,30 @@
 class Api::AttackPatternsController < ApplicationController
-    before_action :set_attack_pattern, only: [:add_comment]
+  before_action :set_attack_pattern, only: [:add_comment]
 
-    def index
-        # binding.pry
-        @attackPatterns = AttackPattern.all
-        render :json => @attackPatterns, include: ['comments', 'external_references', 'kill_chain_phases'] 
+  def index
+    # binding.pry
+    @attackPatterns = AttackPattern.all
+    render :json => @attackPatterns, include: ['comments', 'external_references', 'kill_chain_phases'] 
+  end
+
+  def add_comment
+    @attackPattern.comments.create(name: params[:name],content: params[:content])
+    #binding.pry
+    if @attackPattern.save
+      # binding.pry
+      render :json => @attackPattern, include: ['comments', 'external_references', 'kill_chain_phases'] 
+    else
+      render json: @attackPattern, status: 404
+    end
+  end
+
+  private
+
+    def set_attack_pattern
+      @attackPattern = AttackPattern.find(params[:attack_pattern_id])
     end
 
-    # def create
-    #     @attack_pattern = AttackPattern.new(attack_pattern_params)
-    # end
-
-    def add_comment
-        @attackPattern.comments.create(name: params[:name],content: params[:content])
-      #binding.pry
-      if @attackPattern.save
-        # binding.pry
-        render :json => @attackPattern, include: ['comments', 'external_references', 'kill_chain_phases'] 
-        # render json: [@attackPattern, @attackPattern.comments]
-      else
-        render json: @attackPattern, status: 404
-      end
+    def attack_pattern_params 
+      params.require(:attack_pattern).permit!
     end
-
-    private
-  
-        def set_attack_pattern
-         @attackPattern = AttackPattern.find(params[:attack_pattern_id])
-        end
-
-        def attack_pattern_params 
-            params.require(:attack_pattern).permit!
-        end
-
 end
