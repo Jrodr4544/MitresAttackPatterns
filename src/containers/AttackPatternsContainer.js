7559c67653b4bf789da980139d2c8cd98dae49b3
jrodr4544 <maskdbandit@gmail.com>
2019-05-14 14:12:25 -0400

    removed console logs which are no longer used and modified the router's switch element in order to pass match into container

--- src/containers/AttackPatternsContainer.js
+++ src/containers/AttackPatternsContainer.js
@@ -4,8 +4,8 @@ import { Route, Switch } from 'react-router-dom';
 import { fetchAttackPatterns } from '../actions/index'
 
 import FilterContainer from './FilterContainer';
+import AttackPatternShowContainer from './AttackPatternShowContainer';
 
-import AttacksShow from '../components/AttacksShow';
 import AttackList from '../components/AttacksList';
 //import logo from '../mitre_attack.png'
 
@@ -19,68 +19,66 @@ class AttackPatternsContainer extends Component {
     };
   }
 
-    componentDidMount() {
-        // debugger
-      this.props.fetchAttackPatterns().then( patterns => {
-        console.log(patterns)
-        console.log('COMPONENT DID MOUNT')
-        this.setState({
-          attackPatterns: patterns,
-          filteredAttackPatterns: patterns
-        })
+  componentDidMount() {
+    // debugger
+    this.props.fetchAttackPatterns().then(patterns => {
+      this.setState({
+        attackPatterns: patterns,
+        filteredAttackPatterns: patterns
       })
+    })
+  }
 
-    }
+  handleFilterAttackPatterns(filter) {
+    // debugger
+    // Filter based on data_sources, platforms, and input 
+    const filterPatterns = this.state.attackPatterns.filter(attack =>
+      (attack.x_mitre_data_sources.filter(source => source.includes(filter.datasourceFilter)).length > 1) &&
+      (attack.x_mitre_platforms.filter(platform => platform.includes(filter.platformFilter)).length > 0)
+    );
 
-    handleFilterAttackPatterns(filter) {
-        // debugger
-        // Filter based on data_sources, platforms, and input 
-          const filterPatterns = this.state.attackPatterns.filter( attack => 
-            (attack.x_mitre_data_sources.filter(source => source.includes(filter.datasourceFilter)).length > 0) && 
-            (attack.x_mitre_platforms.filter(platform => platform.includes(filter.platformFilter)).length > 0)
-          );
+    const result = filterPatterns.filter(attack => Object.values(attack).flat().join('').includes(filter['inputFilter']));
 
-          // debugger
-          const result = filterPatterns.filter( attack => Object.values(attack).flat().join('').includes(filter['inputFilter']) ) ;
+    return this.setState({
+      ...this.state,
+      filteredAttackPatterns: result
+    })
 
-          return this.setState({
-            ...this.state,
-            filteredAttackPatterns: result
-          })
+  }
 
-    }
-
-    render() {
-      // debugger
+  render() {
+    debugger
     const { match } = this.props;
 
     return (
       <div>
-{/* Attacks Page Container */}
+        {/* Attacks Page Container */}
 
-        <Switch>  
-         <Route exact path={`${match.url}/:attackId`} component={AttacksShow}/> 
-         <Route path={match.url} render={() => ( 
-          <h3>Select an Attack from the list.</h3>
-         )}/> 
-        </Switch> 
-        
-        <FilterContainer 
+        <Switch>
+          <Route exact path={`${match.url}/:attackId`} render={(match) => <AttackPatternShowContainer match={match} />} />
+          <Route path={match.url} render={() => (
+            <h3>Select an Attack from the list.</h3>
+          )} />
+        </Switch>
+
+        <FilterContainer
           filterAttackPatterns={this.handleFilterAttackPatterns}
         />
-        <AttackList 
+
+        <AttackList
           attackPatterns={this.state.filteredAttackPatterns}
         />
+
       </div>
     )
   }
 }
 
 const mapStateToProps = state => {
-    // debugger
+  debugger
   return {
-     attackPatterns: state.attackPatterns.patterns,
+    attackPatterns: state.attackPatterns.patterns,
   };
 }
 
-export default connect( mapStateToProps , { fetchAttackPatterns })(AttackPatternsContainer);
+export default connect(mapStateToProps, { fetchAttackPatterns })(AttackPatternsContainer);
