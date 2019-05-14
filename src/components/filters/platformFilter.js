9959751729542d96e95503ba4a9dc97e82b35dee
jrodr4544 <maskdbandit@gmail.com>
2019-05-14 14:09:42 -0400

    removed componentClass from select element

--- src/components/filters/platformFilter.js
+++ src/components/filters/platformFilter.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const PlatformFilter = ({platforms, changeFilter}) => {
+const PlatformFilter = ({ platforms, changeFilter }) => {
 
-    const filterOptions = platforms.map((filter, index) => <option key={index} value={filter}>{filter}</option>)
+  const filterOptions = platforms.map((filter, index) => <option key={index} value={filter}>{filter}</option>)
 
-    return (
-          <select componentClass="select" name="platformFilter" onChange={changeFilter}>
-            <option value="">All</option>
-            { filterOptions }
-          </select>
-    );
+  return (
+    <select name="platformFilter" onChange={changeFilter}>
+      <option value="">All</option>
+      {filterOptions}
+    </select>
+  );
 }
 
 export default PlatformFilter;
