5696caf4223e6820320be1135e864ec59e8b2868
jrodr4544 <maskdbandit@gmail.com>
2019-05-14 14:16:03 -0400

    added an action for posting attack pattern comments that will update the state's patterns, and an action if an error occurs

--- src/reducers/attacksReducer.js
+++ src/reducers/attacksReducer.js
@@ -6,6 +6,21 @@ export default (state = {loading: false, patterns: []}, action) => {
 		console.log('LOADING_ATTACK_PATTERNS')
             return { ...state, loading: true }
 
+        case 'POST_ATTACK_PATTERN_COMMENT':
+        debugger
+        console.log('POSTING_ATTACK_PATTERN_COMMENT')
+
+            const updatedPatterns = state.patterns.map( pattern => {
+                if (pattern.id === action.updatedAttackPattern.id) {
+                    return action.updatedAttackPattern 
+                };
+                    return pattern;
+            })
+
+        console.log('UPDATED_AFTER_POSTED_COMMENT')
+
+            return {...state, patterns: updatedPatterns }
+
         case "FETCH_ATTACK_PATTERNS":
         debugger
         console.log('FETCHING_ATTACK_PATTERNS')
@@ -13,10 +28,15 @@ export default (state = {loading: false, patterns: []}, action) => {
             return { ...state, patterns: action.attackPatterns }
 
         case "FETCH_ERROR_GENERATED":
+        debugger
         console.log('FETCH_ERROR_GENERATED')
         console.log(action)
             return { ...state, loading: action } 
-
+    
+        case "POST_ERROR_GENERATED":
+        console.log('POST_ERROR_GENERATED')
+            return { ...state, action }
+        
         default:
             return state
     }
